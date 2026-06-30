883890f40803c0518ff5d4c40c37d8cd10dbd2ed
Mehrad Toudekheil <mehradtoudekheil@gmail.com>
2026-06-30 15:06:44 +0330

    feat(frontend): setup project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// import components 
+import DashboardPage from "./pages/DashboardPage"
+import AuthPage from "./pages/AuthPage"
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-red-500">admin panel</h1>
+    </>
+  )
+}
+
+export default App
