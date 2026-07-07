23c98f193bf176c36af264345cf7315f40befdfd
Mehrad Toudekheil <mehradtoudekheil@gmail.com>
2026-07-07 13:35:50 +0330

    Add Dashboard layouts

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -4,16 +4,24 @@ import Header from "../components/layout/Header";
 
 function DashboardLayout() {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
+    <div className="w-full grid grid-cols-12 bg-slate-50 dark:bg-slate-950 h-screen p-3 gap-x-4">
 
-      <div className="flex flex-1 flex-col">
-        <Header />
+      <aside className="h-full col-span-2 border border-slate-200 dark:border-slate-800 rounded-md">
+        <Sidebar/>
+      </aside>
 
-        <main className="flex-1 overflow-y-auto p-6">
-          <Outlet />
+      <div className="h-full col-span-10 border border-slate-200 dark:border-slate-800 rounded-md">
+
+        <header className="w-full h-18 bg-white dark:bg-slate-900 rounded-tr-md rounded-tl-md">
+          <Header/>
+        </header>
+
+        <main className="w-full h-[90%] p-3">
+          <Outlet/>
         </main>
+
       </div>
+
     </div>
   );
 }
